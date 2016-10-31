e46933957d87b64f9b640a0eeb2e813a6e4f24a3
Loic Blot <loic.blot@unix-experience.fr>
2016-10-31 14:11:46 +0100

    Handle quit command

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,17 @@
 
 extends Sprite
 
-# member variables here, example:
-# var a=2
-# var b="textvar"
-
 func _ready():
 	# Launch scene in full screen mode
-	OS.set_window_fullscreen(true);
+	OS.set_window_fullscreen(true)
 
+	# Tell we accept input key
+	set_process_input(true)
 
+func _input(event):
+	if (event.type == InputEvent.KEY):
+		if (event.is_action("ui_cancel")):
+			quit()
+
+func quit():
+	get_tree().quit()
