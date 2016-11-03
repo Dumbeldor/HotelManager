ab3d5ab669011413fd912cd77376df1bad9c6b77
Loic Blot <loic.blot@unix-experience.fr>
2016-11-03 21:08:01 +0100

    Merge play menu into main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -3,6 +3,13 @@ extends Sprite
 
 var PROJECT = HotelManagerProject.new()
 
+const MAINMENU = {
+	MAIN = 0,
+	PLAY = 1,
+}
+
+var current_menu = MAINMENU.MAIN
+
 func _ready():
 	# Launch scene in full screen mode
 	OS.set_window_fullscreen(true)
@@ -14,7 +21,10 @@ func _ready():
 func _input(event):
 	if (event.type == InputEvent.KEY and event.pressed):
 		if (event.is_action("ui_cancel")):
-			quit()
+			if (current_menu == MAINMENU.MAIN):
+				quit()
+			elif (current_menu == MAINMENU.PLAY):
+				_on_PlayBackButton_released()
 
 func quit():
 	get_tree().quit()
@@ -22,13 +32,21 @@ func quit():
 func _on_QuitButton_released():
 	get_tree().quit()
 
-
 func _on_ProjectNameLabel_draw():
 	get_node("ProjectInfos/ProjectNameLabel").set_text(PROJECT.get_project_name())
 
 func _on_ProjectVersion_draw():
 	get_node("ProjectInfos/ProjectNameLabel/ProjectVersion").set_text(PROJECT.get_full_version())
 
-
 func _on_PlayButton_released():
-	get_tree().change_scene("res://scenes/play_menu.tscn")
+	get_node("MainMenu").hide()
+	get_node("PlayMenu").show()
+	current_menu = MAINMENU.PLAY
+
+func _on_PlayBackButton_released():
+	get_node("PlayMenu").hide()
+	get_node("MainMenu").show()
+	current_menu = MAINMENU.MAIN
+
+func _on_NewGameButton_released():
+	get_tree().change_scene("res://scenes/game_scene.tscn")
