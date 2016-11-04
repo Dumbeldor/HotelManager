d63711662b5b231e78032d9195854ce48e7dbee6
Loic Blot <loic.blot@unix-experience.fr>
2016-11-04 18:36:36 +0100

    Mutiple game scene improvements
    * GUI elements are now linked with camera
    * Add a global menu permitting to return back to main menu

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -4,11 +4,11 @@ extends Sprite
 var game_session = GameSession.new()
 
 func _ready():
-	var day_label = get_node("ControlPane_Top/DayLabel")
+	var day_label = get_node("GameTileMap/Camera2D/ControlPane_Top/DayLabel")
 	if day_label:
 		day_label.set_text("Day: " + str(game_session.get_current_day()))
 
-	var money_label = get_node("ControlPane_Top/DayLabel/MoneyLabel")
+	var money_label = get_node("GameTileMap/Camera2D/ControlPane_Top/DayLabel/MoneyLabel")
 	if money_label:
 		money_label.set_text(str(game_session.get_money()) + " $")
 
@@ -17,3 +17,10 @@ func _ready():
 func _process(delta):
 	var cam = get_node("GameTileMap/Camera2D")
 	cam.global_translate(Vector2(delta * 100, 0))
+
+func _on_ResumeButton_released():
+	get_node("GameTileMap/Camera2D/GameMainMenu").hide()
+
+
+func _on_LeaveGameButton_released():
+	get_tree().change_scene("res://scenes/main_menu.tscn")
