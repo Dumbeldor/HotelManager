773b74087af40e8e633d4edf0776c449600c3a16
Loic Blot <loic.blot@unix-experience.fr>
2016-11-05 11:20:59 +0100

    Mark map limits and draw it with a tile

--- scripts/game_scene.gd
+++ scripts/game_scene.gd
@@ -11,6 +11,10 @@ const GAMEMENU = {
 
 const ZOOMOUT_LIMIT = 8
 const ZOOMIN_LIMIT = 0.55
+const CAMERA_LIMIT_X = 18000
+const CAMERA_LIMIT_Y = CAMERA_LIMIT_X * 1080 / 1920
+const WORLD_LIMIT_X = 180
+const WORLD_LIMIT_Y = WORLD_LIMIT_X * 1080 / 1920
 
 func _input(event):
 	if event.type == InputEvent.KEY and event.pressed:
@@ -28,6 +32,12 @@ func _input(event):
 			zoom_camera(1.11)
 
 func _ready():
+	var cam = get_cam()
+	cam.set_limit(MARGIN_BOTTOM, CAMERA_LIMIT_Y)
+	cam.set_limit(MARGIN_TOP, -CAMERA_LIMIT_Y)
+	cam.set_limit(MARGIN_LEFT, -CAMERA_LIMIT_X)
+	cam.set_limit(MARGIN_RIGHT, CAMERA_LIMIT_X)
+
 	var day_label = get_node("Hud/ControlPane_Top/DayLabel")
 	if day_label:
 		day_label.set_text("Day: " + str(game_session.get_current_day()))
@@ -36,6 +46,14 @@ func _ready():
 	if money_label:
 		money_label.set_text(str(game_session.get_money()) + " $")
 
+	var tilemap = get_node("GameTileMap");
+	for i in range(-WORLD_LIMIT_X, WORLD_LIMIT_X):
+		tilemap.set_cell(i, WORLD_LIMIT_Y, 0)
+		tilemap.set_cell(i, -WORLD_LIMIT_Y, 0)
+
+	for i in range(-WORLD_LIMIT_Y, WORLD_LIMIT_Y):
+		tilemap.set_cell(WORLD_LIMIT_X, i, 0)
+		tilemap.set_cell(-WORLD_LIMIT_X, i, 0)
 	set_process_input(true)
 	set_process(true)
 
@@ -59,11 +77,11 @@ func _process(delta):
 		move_camera(camera_movement)
 
 func move_camera(v):
-	var cam = get_node("GameTileMap/Camera2D")
+	var cam = get_cam()
 	cam.global_translate(v)
 
 func zoom_camera(multiplier):
-	var cam = get_node("GameTileMap/Camera2D")
+	var cam = get_cam()
 	var new_zoom = cam.get_zoom() * multiplier
 	if new_zoom.x >= ZOOMOUT_LIMIT or new_zoom.y >= ZOOMOUT_LIMIT or new_zoom.x <= ZOOMIN_LIMIT or new_zoom.y <= ZOOMIN_LIMIT:
 		return
@@ -72,7 +90,10 @@ func zoom_camera(multiplier):
 func _on_LeaveGameButton_released():
 	get_tree().change_scene("res://scenes/main_menu.tscn")
 
-
 func _hide_game_menu():
 	get_node("Hud/MainMenuLayer/GameMainMenu").hide()
 	current_shown_menu = 0
+
+# node getters
+func get_cam():
+	return get_node("GameTileMap/Camera2D")
