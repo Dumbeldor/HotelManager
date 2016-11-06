fdd94e04e0052f4a54698d478bb441dd9a15d356
Loic Blot <loic.blot@unix-experience.fr>
2016-11-06 13:46:48 +0100

    Trigger mouse click event too when doing motion and mouse is pressed

--- game/scripts/game_map.gd
+++ game/scripts/game_map.gd
@@ -4,7 +4,14 @@ extends GameMap
 func _ready():
 	set_process_input(true)
 
+var input_mouse_pressed = false
+
 func _input(event):
-	if event.type == InputEvent.MOUSE_BUTTON:
-		if event.button_index == BUTTON_LEFT and event.pressed:
+	if event.type == InputEvent.MOUSE_BUTTON and event.button_index == BUTTON_LEFT:
+		if event.is_pressed():
+			input_mouse_pressed = true
 			handle_event_mouse_click(event.pos)
+		else:
+			input_mouse_pressed = false
+	elif event.type == InputEvent.MOUSE_MOTION and input_mouse_pressed:
+		handle_event_mouse_click(event.pos)
