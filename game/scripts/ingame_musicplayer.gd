0658335ea4e613a3adc239c184965524e4201e24
Loic Blot <loic.blot@unix-experience.fr>
2017-01-08 20:14:53 +0100

    Add mainmenu & ingame sounds

---
+++ game/scripts/ingame_musicplayer.gd
@@ -0,0 +1,22 @@
+extends StreamPlayer
+
+var music1
+var music2
+var music3
+
+func _ready():
+	set_process(true)
+	music1 = load("sounds/Gymnopedie No 1.ogg")
+	music2 = load("sounds/Gymnopedie No 2.ogg")
+	music3 = load("sounds/Gymnopedie No 3.ogg")
+
+func _process(delta):
+	if self.is_playing() != true:
+		if (self.get_stream() == music1):
+			self.set_stream(music2)
+		elif self.get_stream() == music2:
+			self.set_stream(music3)
+		else:
+			self.set_stream(music1)
+
+		self.play()
